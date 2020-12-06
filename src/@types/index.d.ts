746a3a42c3e8a92e5d9ffec4b09179b058bc215b
Guilherme <guilhermeht.ramos@gmail.com>
2020-12-06 11:43:16 -0300

    feat: enabling png and json import

---
+++ src/@types/index.d.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
+
+// eslint-disable-next-line
+declare module '*.png' {
+  const content: any;
+  export default content;
+}
